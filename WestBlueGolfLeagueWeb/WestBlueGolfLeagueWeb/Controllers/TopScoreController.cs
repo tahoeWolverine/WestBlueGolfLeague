41ba60455aee149097eb1680517ec5f9541747cd
Polaris <andrew.scott.gerber@gmail.com>
2014-03-18 20:25:46 -0500

    Initial commit of .NET MVC website

---
+++ WestBlueGolfLeagueWeb/WestBlueGolfLeagueWeb/Controllers/TopScoreController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WestBlueGolfLeagueWeb.Models;
+
+namespace WestBlueGolfLeagueWeb.Controllers
+{
+    public class TopScoreController : Controller
+    {
+        private WestBlueGolfDbContext context = new WestBlueGolfDbContext();
+
+        //
+        // GET: /TopScore/
+        public ActionResult Index()
+        {
+            var players = context.Players;
+
+            return View(players.ToList());
+        }
+	}
+}
