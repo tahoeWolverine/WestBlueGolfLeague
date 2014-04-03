ec188d398a001abe3057c17236b2eb06211860ff
Michael Harlow <michael.harlow@thomsonreuters.com>
2014-04-03 17:36:23 -0500

    Adding basic grow code

--- WestBlueGolf/WestBlueGolf/WBProfileDataSource.m
+++ WestBlueGolf/WestBlueGolf/WBProfileDataSource.m
@@ -15,8 +15,12 @@
 
 #define SORT_KEY @"match.teamMatchup.week.date"
 
+#define CELL_HEIGHT 40.0f
+
 @interface WBProfileDataSource () {
 }
+
+@property (strong, nonatomic) NSMutableDictionary *selectedIndexes;
 
 @end
 
@@ -29,8 +33,15 @@
 												 selector:@selector(resetYear)
 													 name:WBYearChangedLoadingFinishedNotification
 												   object:nil];
+		self.selectedIndexes = [[NSMutableDictionary alloc] init];
 	}
 	return self;
+}
+
+- (BOOL)cellIsSelected:(NSIndexPath *)indexPath {
+	// Return whether the cell at the specified index path is selected or not
+	NSNumber *selectedIndex = [self.selectedIndexes objectForKey:indexPath];
+	return [selectedIndex boolValue];
 }
 
 - (void)dealloc {
@@ -76,6 +87,32 @@
     WBResult *result = (WBResult *)object;
 	WBResultTableViewCell *resultCell = (WBResultTableViewCell *)cell;
 	[resultCell configureCellForResult:result];
+}
+
+#pragma mark - Grow code
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+	CGFloat factor = 1.0;
+	if ([self cellIsSelected:indexPath]) {
+		factor = 2.0;
+	}
+	return CELL_HEIGHT * factor;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	// Deselect cell
+	[tableView deselectRowAtIndexPath:indexPath animated:TRUE];
+	
+	// Toggle 'selected' state
+	BOOL isSelected = ![self cellIsSelected:indexPath];
+	
+	// Store cell 'selected' state keyed on indexPath
+	NSNumber *selectedIndex = [NSNumber numberWithBool:isSelected];
+	[self.selectedIndexes setObject:selectedIndex forKey:indexPath];
+	
+	// This is where magic happens...
+	[tableView beginUpdates];
+	[tableView endUpdates];
 }
 
 @end
