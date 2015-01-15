f0f520da7468057d4782cd2755bfe0a51075af4d
DigitalZebra <DigitalZebra@users.noreply.github.com>
2015-01-14 19:32:33 -0600

    starting admin stuff, lots of learning

---
+++ WestBlueGolfLeagueWeb/WestBlueGolfLeagueWeb/Controllers/YearAdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using WestBlueGolfLeagueWeb.Models.Entities;
+using System.Threading.Tasks;
+
+namespace WestBlueGolfLeagueWeb.Controllers
+{
+	[Authorize]
+	public class YearAdminController : WestBlueDbMvcController
+	{
+		public YearAdminController()
+			: base(true)
+		{
+		}
+
+		// YearAdmin/
+		public async Task<ActionResult> Index()
+		{
+			List<year> allYears = await this.Db.years.OrderBy(x => x.value).ToListAsync();
+			return View(allYears);
+		}
+	}
+}
