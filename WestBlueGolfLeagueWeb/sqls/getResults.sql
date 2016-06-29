8325a3f34cd7427eed9d70b21e6cbf8af8fff8e0
DigitalZebra <DigitalZebra@users.noreply.github.com>
2016-06-29 16:52:39 -0500

    sqls

--- WestBlueGolfLeagueWeb/sqls/getResults.sql
+++ WestBlueGolfLeagueWeb/sqls/getResults.sql
@@ -7,7 +7,7 @@ select
 	matches.*
 from
     teammatchup tm
-        left outer join
+        inner join
     (select 
         m.id as matchId,
 		m.matchOrder,
@@ -30,4 +30,43 @@ from
     inner join `year` y ON y.id = w.yearId
     WHERE
         y.value = 2016) yy ON yy.joinedWeekId = tm.weekId;
+		
+
+/*With players:	*/	
+select 
+    /*tm.id as teamMatchupId*/
+	tm.id,
+	tm.matchComplete,
+	tm.weekId,
+	tm.playoffType,
+	matches.*
+from
+    teammatchup tm
+        inner join
+    (select 
+        m.id as matchId,
+		m.matchOrder,
+		m.teamMatchupId,
+		r.score,
+		r.points,
+		r.teamId,
+		r.playerId,
+		r.id as resultId,
+		r.priorHandicap,
+		r.scoreVariant,
+		p.name
+    from
+        `match` m
+    left outer join result r ON m.id = r.matchId
+	inner join player p ON r.playerId = p.id
+) matches ON matches.teamMatchupId = tm.id
+        inner join
+    (select 
+        w.id as joinedWeekId
+    FROM
+        week w
+    inner join `year` y ON y.id = w.yearId
+    WHERE
+        y.value = 2016) yy ON yy.joinedWeekId = tm.weekId;
+		
 		
