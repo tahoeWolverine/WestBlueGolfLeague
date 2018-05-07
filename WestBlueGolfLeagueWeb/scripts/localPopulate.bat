1bff065bfb3b7cf261eec37358e5b7edc75f5e53
Mike <harlomic@gmail.com>
2018-05-07 00:36:34 -0500

    Backing up week 1 and also new admins and removal of A. Caswell from 2018

--- WestBlueGolfLeagueWeb/scripts/localPopulate.bat
+++ WestBlueGolfLeagueWeb/scripts/localPopulate.bat
@@ -6,7 +6,7 @@ set /p password=
 echo Your username is %usern%
 echo Your Group is %password%
 echo Loading database...
-call "C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql.exe" --user=%usern% --password=%password% < 04_30_2018.sql
+call "C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql.exe" --user=%usern% --password=%password% < 05_06_2018-post-changes.sql
 echo Creating WestBlue users...
 call "C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql.exe" --user=%usern% --password=%password% < createSqlUsers.sql
 pause
