e4f5ee857888ae9dd1976492f6bfaaf70a77d386
Mike <harlomic@gmail.com>
2021-08-17 22:53:06 -0500

    week 15 backup; pre-playoff match adds

--- WestBlueGolfLeagueWeb/scripts/localPopulate.bat
+++ WestBlueGolfLeagueWeb/scripts/localPopulate.bat
@@ -6,7 +6,7 @@ set /p password=
 echo Your username is %usern%
 echo Your Group is %password%
 echo Loading database...
-call "C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql.exe" --user=%usern% --password=%password% < 08_11_2021-playoffprep.sql
+call "C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql.exe" --user=%usern% --password=%password% < 08_17_2021-regular.sql
 echo Creating WestBlue users...
 call "C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql.exe" --user=%usern% --password=%password% < createSqlUsers.sql
 pause
