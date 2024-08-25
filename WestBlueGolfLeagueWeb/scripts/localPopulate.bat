098678af2c430ff7ef16f5eece18c13ca54e6264
Michael Harlow <harlomic@gmail.com>
2024-08-25 17:02:41 -0500

    Playoff matchups added

--- WestBlueGolfLeagueWeb/scripts/localPopulate.bat
+++ WestBlueGolfLeagueWeb/scripts/localPopulate.bat
@@ -6,7 +6,7 @@ set /p password=
 echo Your username is %usern%
 echo Your Group is %password%
 echo Loading database...
-call "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --user=%usern% --password=%password% < 08_25_2024.sql
+call "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --user=%usern% --password=%password% < 08_25_2024-playoff.sql
 echo Creating WestBlue users...
 call "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --user=%usern% --password=%password% < createSqlUsers.sql
 pause
