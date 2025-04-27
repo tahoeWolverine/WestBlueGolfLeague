a9776f99cc7f2b6f437e008992191935f7291ce9
Michael Harlow <harlomic@gmail.com>
2025-04-27 15:28:24 -0500

    Added new users and captains - backed up

--- WestBlueGolfLeagueWeb/scripts/localPopulate.bat
+++ WestBlueGolfLeagueWeb/scripts/localPopulate.bat
@@ -6,7 +6,7 @@ set /p password=
 echo Your username is %usern%
 echo Your Group is %password%
 echo Loading database...
-call "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --user=%usern% --password=%password% < 03_15_2025.sql
+call "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --user=%usern% --password=%password% < 04_27_2025-users.sql
 echo Creating WestBlue users...
 call "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --user=%usern% --password=%password% < createSqlUsers.sql
 pause
